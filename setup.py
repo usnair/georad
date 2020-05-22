95d6e7e554a310c5682009a3d966207860500c0c
Udaysankar Nair <nairu@uah.edu>
2020-05-22 00:50:44 -0500

    files added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='georad',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com/usnair/georad',
+    author='Udaysankar Nair',
+    author_email='nairu@uah.edu',
+    license='unlicense',
+    packages=['MyPackage'],
+    zip_safe=False
+)
