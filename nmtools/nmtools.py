3f7a85f074231fd6089cd1e71c660370189d8d76
Udaysankar Nair <nairu@uah.edu>
2020-05-23 13:42:36 -0500

    added my package

--- nmtools/nmtools.py
+++ nmtools/nmtools.py
@@ -1,93 +1,27 @@
-from IPython.display import display
-from ipywidgets import HBox, VBox, Button,Layout, HTML,Output,\
-                       GridspecLayout,Dropdown,GridBox,Textarea,Text
-import openaq
-import pandas as pd
+from pydap.client import open_url
+from pydap.cas.urs import setup_session
+import numpy as np
+from netCDF4 import Dataset
 import matplotlib.pyplot as plt
+from ipywidgets import Button,Output
+from datetime import datetime, timedelta, date
+from dateutil import rrule
+import ipywidgets as widgets
+from IPython.display import display
+from ipywidgets import HBox, VBox, Button,Layout, HTML,Output,GridspecLayout,Dropdown,GridBox,Textarea,Text,Password
 import seaborn as sns
+from windrose import WindroseAxes
 import sys
-sys.path.append("../openaqgui")
-from openaqgui import OpenAQGui
+sys.path.append("../panelobj")
+from panelobj import PanelObject
 
 
 #from ..openaqgui.openaqgui import OpenAQGui
 
-class QueryOpenAq(OpenAQGui):
-  def __init__(self,*args,**kwargs):
-    OpenAQGui.__init__(self,*args, **kwargs)
 
-  def getCP(self):
-
-    self.title = 'Query OpenAQ'
-
-    self.setLabel()
+class MERRA_WindRose(PanelObject):
     
-    self.showInfo = Button(description='Query',disabled=False,
-                           layout={'width':'auto','border':'3px outset'})
-    
-    self.showInfo.on_click(self.showQuery)
-
-    self.inpUSR.children+= (VBox([self.showInfo],layout={'overflow':'visible'}),)
-
-
-    return self.cp
-
-  def showQuery(self,b):
-      try:
-        df = self.locations.loc[self.locations['location']==self.location].T
-        with self.out_cp:
-          self.out_cp.clear_output()
-          display(df)
-      except Exception:
-        with self.out_cp:
-          self.out_cp.clear_output()
-          print('Error getting data')
-
-class PlotOpenAq(OpenAQGui):
-  def __init__(self,*args,**kwargs):
-    OpenAQGui.__init__(self,*args, **kwargs)
-
-  def getCP(self):
-
-    self.title = 'Query OpenAQ'
-    
-    self.setLabel()
-    
-    self.plotStn = Button(description='Plot',disabled=False,
-                           layout={'width':'150px','overflow':'visible','border':'3px outset'})
-    
-    self.plotStn.on_click(self.plotObs)
-
-    self.inpUSR.children+= (VBox([self.plotStn],layout={'overflow':'visible'}),)
-
-    return self.cp
-  
-  def plotObs(self,b):
-    try:
-      res = self.api.measurements(location=self.location,parameter='pm25', limit=10000,df=True)
-      plot_obs = True
-    except Exception:
-      print("Error getting data")
-      plot_obs = False
-      return False
-    with self.out_cp:
-      plt.ioff()
-      fig,ax = plt.subplots(1, figsize=(12, 6))
-      sns.set(style="ticks", font_scale=1.35)
-      for group, df in res.groupby('location'):
-        _df = df.query("value >= 0.0").resample('12h').mean()
-        # Convert from ppm to ppb
-        _df['value'] *= 1e0
-        # Plot the data
-        _df.value.plot(ax=ax, label=group)
-      ax.legend(loc='best')
-      ax.set_ylim(0, None)
-      ax.set_ylabel("$PM2.5 \; [\mu m^-3]$", fontsize=18)
-      ax.set_xlabel("")
-      # move the legend to the side
-      plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-      sns.despine(offset=5)
-      self.out_cp.clear_output()
-      #display(fig)
-      plt.show()
-      return True
+    def __init__(self,*args,**kwargs):
+        self.title = "MERRA2 Wind Rose"
+        self.accList =['NASA Earth Data']
+        PanelObject.__init__(self,*args, **kwargs)
