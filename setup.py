59cd07bc1c1abce51c461c984fcebb782db6c553
Udaysankar Nair <nairu@uah.edu>
2020-05-23 12:04:29 -0500

    added my package

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author='Udaysankar Nair',
     author_email='nairu@uah.edu',
     license='unlicense',
-    packages=['panelobj','openaqgui'],
+    packages=['panelobj','openaqgui','oaqtools'],
     zip_safe=False
 )
